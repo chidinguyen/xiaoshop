9f54fbd312377223357dd4407aae932392ee4e47
ditvpro147 <nguyenchidi84@gmail.com>
2022-02-28 21:28:40 +0700

    update 3

---
+++ XiaoProject/XiaoWebParent/XiaoFrontEnd/src/main/java/com/xiao/site/XiaoFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.xiao.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaoFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XiaoFrontEndApplication.class, args);
+	}
+
+}
